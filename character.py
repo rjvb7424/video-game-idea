59d720b211bdea5ff127828c5bf01b28ad8cb193
rjvb7424 <rjvb7424@leeds.ac.uk>
2025-12-18 23:47:38 +0000

    Added a __repr__ method to the Character class

--- character.py
+++ character.py
@@ -3,3 +3,6 @@ class Character:
         self.fname = fname
         self.lname = lname
         self.age = age
+
+    def __repr__(self):
+        return f"{self.fname.capitalize()} {self.lname.capitalize()}"
